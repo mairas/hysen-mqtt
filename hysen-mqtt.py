183beaae876bc92298e8d124746c7aad2d6ef4a3
Matti Airas <mairas@iki.fi>
2019-12-28 20:41:31 +0200

    Create separate clients per Hysen device

--- hysen-mqtt.py
+++ hysen-mqtt.py
@@ -27,35 +27,70 @@ def get_config():
     return host, port, user, password
 
 
-def set_target_temperature(device, payload):
-    temperature = float(payload)
-    device.set_temp(temperature)
+class HysenMQTTConnector:
+    def __init__(self, device, host, port, user, password):
+        self._device = device
+        self._device_id = self.get_device_id()
+        self._mqtt_client = self._build_mqtt_client(host, port, user, password)
 
+    def _build_mqtt_client(self, host, port, user, password):
+        client = mqtt.Client()
+        client.on_connect = self._on_connect
+        client.on_message = self._on_message
+        self._set_last_will(client)
+        client.username_pw_set(user, password)
+        client.connect(host, port)
+        client.loop_start()
+        return client
 
-def set_thermostat_mode(device, payload):
-    power = {
-        "off": 0,
-        "heat": 1
-    }
-    device.set_power(power[payload])
+    def _on_connect(self, client, userdata, flags, rc):
+        print("Connected with result code {}".format(rc))
+        self.subscribe_topics()
+        self.publish_configuration()        
+        self.publish_available()
 
+    def _on_message(self, client, userdata, message):
+        payload = message.payload.decode('utf-8')
+        topic = message.topic
 
-def get_device_id(device):
-    dev_type = device.type
-    dev_mac_str = ''.join('{:02x}'.format(x) for x in device.mac)
-    return '{}_{}'.format(dev_type.lower().replace(' ', '_'), dev_mac_str)
+        match = re.match(r"^homeassistant/climate/{}/(.*?)$".format(
+            self._device_id), topic)
+        command = match.group(1)
+        
+        handlers = {
+            "targetTempCmd": self.set_target_temperature,
+            "thermostatModeCmd": self.set_thermostat_mode
+        }
 
+        print("Calling handler for {}({})".format(command, payload))
+        handlers[command](payload)
+        self.publish_state()
 
-def publish_configuration(devices, mqtt_client):
-    for dev_id, device in devices.items():
-        config_topic = 'homeassistant/climate/{}/config'.format(dev_id)
-        device_mac = dev_id.rsplit('_', 1)[-1]
-        device_name = '{} {}'.format(device.type, device_mac)
-        base = 'homeassistant/climate/{}'.format(dev_id)
+    def set_target_temperature(self, payload):
+        temperature = float(payload)
+        self._device.set_temp(temperature)
+
+    def set_thermostat_mode(self, payload):
+        power = {
+            "off": 0,
+            "heat": 1
+        }
+        self._device.set_power(power[payload])
+
+    def get_device_id(self):
+        dev_type = self._device.type
+        dev_mac_str = ''.join('{:02x}'.format(x) for x in self._device.mac)
+        return '{}_{}'.format(dev_type.lower().replace(' ', '_'), dev_mac_str)
+
+    def publish_configuration(self):
+        config_topic = 'homeassistant/climate/{}/config'.format(self._device_id)
+        device_mac = self._device_id.rsplit('_', 1)[-1]
+        device_name = '{} {}'.format(self._device.type, device_mac)
+        base = 'homeassistant/climate/{}'.format(self._device_id)
         device_payload = {
             "identifiers": [ device_mac ],
             "name": device_name,
-            "model": device.type,
+            "model": self._device.type,
             "manufacturer": "Broadlink"
         }
         config_payload = {
@@ -81,55 +116,85 @@ def publish_configuration(devices, mqtt_client):
 
         print('Publishing config: ', config_payload)
 
-        mqtt_client.publish(config_topic, 
-                            json.dumps(config_payload), 
-                            retain=True)
+        self._mqtt_client.publish(config_topic, 
+                                  json.dumps(config_payload), 
+                                  retain=True)
 
+    def publish_state(self):
+        state_topic = 'homeassistant/climate/{}/state'.format(self._device_id)
+        status = self._device.get_full_status()
+        mode = "heat" if status["power"] else "off"
+        target_temp = str(status["thermostat_temp"])
+        current_temp = str(status["external_temp"])
+        state_payload = {
+            "mode": mode,
+            "target_temp": target_temp,
+            "current_temp": current_temp
+        }
 
-def publish_state(dev_id, device, mqtt_client):
-    state_topic = 'homeassistant/climate/{}/state'.format(dev_id)
-    status = device.get_full_status()
-    mode = "heat" if status["power"] else "off"
-    target_temp = str(status["thermostat_temp"])
-    current_temp = str(status["external_temp"])
-    state_payload = {
-        "mode": mode,
-        "target_temp": target_temp,
-        "current_temp": current_temp
-    }
+        self._mqtt_client.publish(state_topic, json.dumps(state_payload))
 
-    mqtt_client.publish(state_topic, json.dumps(state_payload))
-
-
-def publish_available(devices, mqtt_client):
-    for dev_id in devices:
-        availability_topic = 'homeassistant/climate/{}/available'.format(dev_id)
+    def publish_available(self):
+        availability_topic = 'homeassistant/climate/{}/available'.format(
+            self._device_id)
         availability_payload = 'online'
-        mqtt_client.publish(
+        self._mqtt_client.publish(
             availability_topic, availability_payload, retain=True)
 
-
-def subscribe_topics(devices, mqtt_client):
-    topics = ["targetTempCmd", "thermostatModeCmd"]
-    for dev_id in devices:
-        base = "homeassistant/climate/{}/".format(dev_id)
+    def subscribe_topics(self):
+        topics = ["targetTempCmd", "thermostatModeCmd"]
+        base = "homeassistant/climate/{}/".format(self._device_id)
         for topic in topics:
             full_topic = base + topic
             print("Subscribing to {}".format(full_topic))
-            mqtt_client.subscribe(full_topic, 0)
+            self._mqtt_client.subscribe(full_topic, 0)
 
+    async def set_time(self):
+        cur_time = time.localtime()
+        self._device.set_time(
+            cur_time.tm_hour, 
+            cur_time.tm_min, 
+            cur_time.tm_sec,
+            cur_time.tm_wday+1)
 
-async def set_time(device):
-    cur_time = time.localtime()
-    device.set_time(cur_time.tm_hour, cur_time.tm_min, cur_time.tm_sec,
-                    cur_time.tm_wday+1)
+    async def set_time_coro(self):
+        while True:
+            sleep_future = asyncio.sleep(30*60)
+            await self.set_time()
+            await sleep_future
 
+    def _set_last_will(self, client):
+        will_topic = 'homeassistant/climate/{}/available'.format(
+            self._device_id)
+        will_payload = 'offline'
+        client.will_set(will_topic, will_payload, retain=True)
 
-async def set_time_coro(device):
-    while True:
-        sleep_future = asyncio.sleep(30*60)
-        await set_time(device)
-        await sleep_future
+    async def publish_config_coro(self):
+        while True:
+            await asyncio.sleep(10*60)
+            self.publish_configuration()
+
+    async def publish_available_coro(self):
+        while True:
+            await asyncio.sleep(10*60)
+            self.publish_available()
+
+    async def publish_state_coro(self):
+        while True:
+            sleep_future = asyncio.sleep(POLL_INTERVAL)
+            self.publish_state()
+            await sleep_future
+
+    async def start_tasks(self):
+        tasks = []
+
+        tasks.append(asyncio.create_task(self.publish_config_coro()))
+        tasks.append(asyncio.create_task(self.publish_available_coro()))
+        tasks.append(asyncio.create_task(self.publish_state_coro()))
+        tasks.append(asyncio.create_task(self.set_time_coro()))
+
+        for task in tasks:
+            await task
 
 
 async def get_devices(timeout=10):
@@ -137,103 +202,25 @@ async def get_devices(timeout=10):
     hysen_devices = [dev for dev in all_devices 
                      if isinstance(dev, broadlink.hysen)]
 
-    dev_dict = {get_device_id(device): device for device in hysen_devices}
-    
-    return dev_dict
-
-
-# FIXME: there is only one will per connection, so this will only result in
-#        one last will being set
-async def set_device_wills(devices, mqtt_client):
-    for dev_id in devices:
-        will_topic = 'homeassistant/climate/{}/available'.format(dev_id)
-        will_payload = 'offline'
-        mqtt_client.will_set(will_topic, will_payload, retain=True)
-
-
-async def publish_config_coro(devices, mqtt_client):
-    while True:
-        await asyncio.sleep(10*60)
-        publish_configuration(devices, mqtt_client)
-
-
-async def publish_available_coro(devices, mqtt_client):
-    while True:
-        await asyncio.sleep(10*60)
-        publish_available(devices, mqtt_client)
-
-
-async def publish_state_coro(dev_id, device, mqtt_client):
-    while True:
-        sleep_future = asyncio.sleep(POLL_INTERVAL)
-        publish_state(dev_id, device, mqtt_client)
-        await sleep_future
-
-
-async def start_tasks(devices, mqtt_client):
-    mqtt_client.loop_start()
-
-    tasks = []
-
-    for dev_id, device in devices.items():
-        tasks.append(asyncio.create_task(
-            publish_config_coro(devices, mqtt_client)))
-        tasks.append(asyncio.create_task(
-            publish_available_coro(devices, mqtt_client)))
-        tasks.append(asyncio.create_task(
-            publish_state_coro(dev_id, device, mqtt_client)))
-        tasks.append(asyncio.create_task(
-            set_time_coro(device)))
-
-    for task in tasks:
-        await task
+    return hysen_devices
 
 
 async def main():
-    mqtt_host, mqtt_port, mqtt_user, mqtt_password = get_config()
+    host, port, user, password= get_config()
 
     devices = await get_devices()
 
-    for device in devices.values():
+    connectors = []
+    tasks = []
+    for device in devices:
         device.auth()
+        connector = HysenMQTTConnector(device, host, port, user, password)
+        connectors.append(connector)
+        tasks.append(asyncio.create_task(connector.start_tasks()))
 
-    mqtt_client = mqtt.Client()
-
-    def on_connect(client, userdata, flags, rc):
-        print("Connected with result code {}".format(rc))
-        subscribe_topics(devices, mqtt_client)
-        publish_configuration(devices, mqtt_client)        
-        publish_available(devices, mqtt_client)
-
-    mqtt_client.on_connect = on_connect
-
-    def on_message(client, userdata, message):
-        payload = message.payload.decode('utf-8')
-        topic = message.topic
-
-        match = re.match(r"^homeassistant/climate/(\w*?)/(.*?)$", topic)
-        dev_id = match.group(1)
-        command = match.group(2)
-        
-        handlers = {
-            "targetTempCmd": set_target_temperature,
-            "thermostatModeCmd": set_thermostat_mode
-        }
-
-        print("Calling handler for {}({}, {})".format(command, dev_id, payload))
-        handlers[command](devices[dev_id], payload)
-        publish_state(dev_id, devices[dev_id], mqtt_client)
-
-    mqtt_client.on_message = on_message
-
-    await set_device_wills(devices, mqtt_client)
-
-    mqtt_client.username_pw_set(mqtt_user, mqtt_password)
-
-    mqtt_client.connect(mqtt_host, mqtt_port)
-
-    await start_tasks(devices, mqtt_client)
-
+    # these tasks should never finish
+    for task in tasks:
+        await task
 
 if __name__ == '__main__':
     asyncio.run(main())
