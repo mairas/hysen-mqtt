8842536fee86d87f52054d5bc2feea7dbeb4f78f
Matti Airas <mairas@iki.fi>
2019-12-30 21:18:46 +0200

    Support LOCAL_IP_ADDR; restart the daemon if it exits

--- run.sh
+++ run.sh
@@ -7,6 +7,12 @@ export MQTT_HOST=$(jq --raw-output .mqtt_host $CONFIG_PATH)
 export MQTT_PORT=$(jq --raw-output .mqtt_port $CONFIG_PATH)
 export MQTT_USER=$(jq --raw-output .mqtt_user $CONFIG_PATH)
 export MQTT_PASSWORD=$(jq --raw-output .mqtt_password $CONFIG_PATH)
+export LOCAL_IP_ADDR=$(jq --raw-output .local_ip_address $CONFIG_PATH)
 
 # run the executable
-python3 /hysen-mqtt.py
+while true; do
+    echo "Starting Hysen MQTT daemon"
+    python3 /hysen-mqtt.py
+    echo "Sleeping before restarting"
+    sleep 10
+done
